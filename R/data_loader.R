493be5fa3b28332e31befa7c72575b47c6872c09
Quang Nguyen <quangpmnguyen@gmail.com>
2021-03-15 18:27:25 -0400

    Adding cfr calculations

--- R/data_loader.R
+++ R/data_loader.R
@@ -11,13 +11,15 @@ data_loader_ui <- function(id, label = "Loading Data"){
       sidebarPanel(
         selectInput(ns("state"), "Select State", choices = c(state.name, "Washington DC")),
         selectInput(ns("data_type"), "Select Data type", choices = c("Daily", "Total")),
+        sliderInput(ns("lag"),"Lag (in days) for cases in CFR calculations", min = 0, max = 30, value = 0),
         #checkboxGroupInput("dat", "Select Data Type", choices = data_streams, selected = "Deaths CTP"),
         actionButton(ns("load"), "Load Data")
       ), 
       mainPanel(
         tabsetPanel(type = "tabs",
                     tabPanel("Plot", plotlyOutput(ns("main_plot"))),
-                    tabPanel("Annotation", gt_output(ns("annotation")))
+                    tabPanel("Annotation", gt_output(ns("annotation"))),
+                    tabPanel("Case Fatality Ratio", plotlyOutput(ns("cfr_plot")))
         )
         
       )
@@ -30,7 +32,7 @@ data_loader_server <- function(id){
     id, 
     function(input, output, session){
       dat <- eventReactive(input$load, {
-        types <- c("ctp", "nchs", "cdc")
+        types <- c("ctp", "nchs", "cdc", "cdc_cases")
         df_list <- vector(mode = "list", length = length(types))
         withProgress(message = "Loading data...", {
           for (i in 1:length(types)){
@@ -51,7 +53,8 @@ data_loader_server <- function(id){
       states <- data.frame(abb = c(state.abb, "DC"), name = c(state.name, "Washington DC"))
       annotations <- retr_airtable() %>%  inner_join(states, by = "abb") %>% 
         select(c(name, metric, annotation, evidence))
-
+      
+      # Generating annotation table from airtable 
       table <- reactive({
         annotations %>% filter(name == input$state) %>% 
           rename(c("State" = "name", "Metric" = "metric", "Annotation" = "annotation", 
@@ -78,6 +81,7 @@ data_loader_server <- function(id){
           )
           
       }) 
+      
       df <- reactive({
         dat() %>% 
           pivot_longer(ends_with(filt_string()), names_to = "data_sets", values_to = "deaths") %>%
@@ -85,6 +89,17 @@ data_loader_server <- function(id){
           mutate(data_sets = str_remove(data_sets, paste0("_", filt_string()))) %>% 
           mutate(data_sets = str_to_upper(data_sets)) %>% 
           filter(name == input$state) %>% filter(!is.na(deaths))
+      })
+      
+      df_cfr <- reactive({
+        dat() %>% filter(name == input$state) %>% 
+          select(c(date, abb, name, contains("tot"))) %>%
+          mutate(cases = lag(cdc_tot_cases, n = input$lag)) %>% 
+          mutate(across(ends_with("tot_deaths"), ~(.x/cases)*100)) %>%
+          pivot_longer(ends_with("tot_deaths"), names_to = "data_sets", values_to = "cfr") %>% 
+          mutate(data_sets = str_remove(data_sets, "tot_deaths")) %>%
+          mutate(data_sets = str_to_upper(data_sets)) %>%
+          filter(!is.na(cfr))
       })
       
       output$main_plot <- renderPlotly({
@@ -101,6 +116,17 @@ data_loader_server <- function(id){
           fig
       })
       output$annotation <- render_gt(table())
+      output$cfr_plot <- renderPlotly({
+        fig <- ggplot(df_cfr(), aes(x = date, y = cfr, col = data_sets)) + 
+          geom_line(size = 1.5, alpha = 0.8) + theme_bw() + 
+          scale_color_manual(values = c("#0091ea", "#6164ba", "#ffad4a")) + 
+          labs(x = "Date", y = "Case Fatality Ratio (%)", 
+               title = "Case Fatality Ratio (%) by MMWR week ending date",
+               subtitle = glue("{state} \n Lag: {lag} days", state = input$state, lag = input$lag),
+               col = "Data Sets", 
+               caption = "Source: CDC Data Tracker, NCHS Provisional Death Counts, The COVID Tracking Project") + 
+          theme_peter()
+      })
     }  
   )
 }
@@ -109,7 +135,7 @@ data_loader_server <- function(id){
 # individual functions  
 
 # wrapper function for loading data 
-load_data <- function(type = c("nchs", "ctp", "cdc"), cutoff_month = 3){
+load_data <- function(type = c("nchs", "ctp", "cdc", "cdc_cases"), cutoff_month = 3){
   type <- match.arg(type)
   states <- data.frame(abb = c(state.abb, "DC"), name = c(state.name, "Washington DC"))
   df <- do.call(type, args = list(states = states, cutoff_month=cutoff_month), envir = environment())
@@ -162,6 +188,21 @@ ctp <- function(states, cutoff_month){
              "ctp_tot_probable" = "deathProbable", "abb" = "state", "ctp_new_deaths" = "deathIncrease")) %>%
     inner_join(states, by = "abb") %>% 
     arrange(name,date) %>% filter(date >= "2020-03-01") %>% 
+    filter(date <= "2021-03-07")
+  return(df)
+}
+
+cdc_cases <- function(states, cutoff_month){
+  access <- readRDS(file = "store.rds")
+  df <- read.socrata(
+    "https://data.cdc.gov/resource/9mfq-cb36.json", app_token = access$app_token, 
+    password = access$password, email = access$email
+  )
+  df <- df %>% as_tibble() %>% rename(c("date" = "submission_date", "abb" = "state")) %>%
+    select(date, abb, tot_cases, new_case) %>% inner_join(states, by = "abb") %>%
+    mutate(tot_cases = as.numeric(tot_cases), new_case = as.numeric(new_case), date = as_date(date)) %>%
+    rename("cdc_new_cases" = "new_case", "cdc_tot_cases" = "tot_cases") %>%
+    arrange(name,date) %>% filter(month(date) >= cutoff_month & year(date) == 2020) %>% 
     filter(date <= "2021-03-07")
   return(df)
 }
--- server.R
+++ server.R
@@ -9,30 +9,7 @@ library(glue)
 
 function(input, output, session){
   data_loader_server("state_data")
-  # data <- eventReactive(input$load,{
-  #   types <- c("nyt", "ctp", "nchs")
-  #   dfs <- vector(mode = "list", length = length(types))
-  #   withProgress(message = "Loading data...", {
-  #     for (i in 1:length(types)){
-  #       incProgress(1/length(types), detail = paste("Loading data ", types[i]))
-  #       dfs[[i]] <- load_data(type = types[i])
-  #     }
-  #   })
-  #   dfs <- dfs %>% reduce(left_join, by = c("date", "state")) 
-  #   dfs <- dfs %>% pivot_longer(starts_with("Death"), names_to = "data_type")
-  #   dfs
-  # })
-  # 
-  # output$main_plot <- renderPlotly({
-  #   df <- data() %>% filter(data_type %in% input$dat, state == input$state)
-  #   fig <- ggplot(df, aes(x = date, y = value, color = data_type)) + 
-  #     geom_line() + theme_bw() + geom_point() + scale_color_nejm() +  
-  #     labs(x = "Date", y = "Cumulative Deaths", color = "Data Types", 
-  #          title = "Cumulative deaths by date and data source")
-  #   fig <- ggplotly(fig)
-  #   fig
-  # })
-  # 
+  
   # canvas_plt <- eventReactive(input$load_canvas, {
   #   df <- data() %>% filter(data_type %in% input$dat_2) %>% filter(state %in% state.name)
   #   fig <- ggplot(df, aes(x = date, y = value, color = data_type)) + 
